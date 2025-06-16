2c0423ee376b56e9de0c64c7b760fdcd49b13eb2
Varun-redsage <123965192+VarunChowdary0@users.noreply.github.com>
2025-06-16 13:24:20 +0530

    fixes: did few layout fixes

--- src/components/inbox/MainInboxComp.tsx
+++ src/components/inbox/MainInboxComp.tsx
@@ -1,19 +1,11 @@
 "use client"
 
-import React, { Suspense, useEffect } from 'react'
-import { useSelector } from 'react-redux'
-import { RootState } from '@/redux/store'
-import ChangeThemeColor from '@/lib/ChangeThemeColor'
+import React, { Suspense} from 'react'
 import { Loader2 } from 'lucide-react'
 import InboxMapper from './widgets/InBoxMapper'
 
 const MainInboxComp = () => {
-  const isDarkMode = useSelector((state:RootState)=> state.booleans.isDarkMode)
-  useEffect(()=>{
-    if(isDarkMode){
-      ChangeThemeColor("#171717");
-    }
-  },[isDarkMode])
+
   return (
     <div className=' flex h-full max-sm:p-0 w-full max-sm:py-0 max-sm:mt-0 gap-10 flex-col
      snap-mandatory snap-y  pb-5 '>
